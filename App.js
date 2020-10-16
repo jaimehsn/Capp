86ffb3c7ff53ac8ada3822b267f4cc27270a2b8e
Jaime Hidalgo Sanchez-Noriega <jaimehsn@gmail.com>
2020-10-16 19:23:41 +0200

    react-native-vector-icons done

--- App.js
+++ App.js
@@ -7,10 +7,11 @@
  */
 
 import React, { Component, useState } from 'react';
-import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, FlatList, Alert, TouchableWithoutFeedback, Keyboard,  } from 'react-native';
 import Header from './components/header'
 import TodoItem from './components/todoItem'
 import AddTodo from './components/addTodo'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 const App = () => {
 
@@ -24,33 +25,47 @@ const App = () => {
   }
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [
-        ...prevTodos,
-        {text: text, key: ((prevTodos.length)+1).toString()},
-      ]
-    })
+
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          ...prevTodos,
+          { text: text, key: ((prevTodos.length) + 1).toString() },
+        ]
+      })
+    } else {
+      Alert.alert('OOPS!', 'La tarea tiene que tener más de 3 letras.', [
+        { text: 'Ok', onPress: () => console.log('alert closed') }
+      ])
+    }
+
+
 
   }
 
   return (
-    <View style={styles.container}>
-      {/* header */}
-      <Header />
-      <View style={styles.content}>
-        {/* form */}
-        <AddTodo submitHandler={submitHandler} />
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem={({ item }) => (
-              <TodoItem item={item} pressHandler={pressHandler} />
-            )} />
+    // <Sandbox/>
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      console.log("dismissed keyboard")
+    }}>
+      <View style={styles.container}>
+        {/* header */}
+        <Header />
+        <Icon name="check-circle" size={30} color="#000" />
+        <View style={styles.content}>
+          {/* form */}
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )} />
+          </View>
         </View>
-
       </View>
-
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -58,14 +73,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    //alignItems: 'center',
-    //justifyContent: 'center',
   },
   content: {
     padding: 40,
+    
+    flex:1
   },
   list: {
     marginTop: 20,
+    
+    flex:1,
   }
 });
 
